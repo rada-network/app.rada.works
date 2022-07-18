646db2ad51533de7c9eef3739268864ae4b9d6b4
Kien Duong <kienduong.hust@gmail.com>
2022-07-18 15:04:33 +0700

    add component joblisting

--- src/components/templates/CreateJobTmpl.js
+++ src/components/templates/CreateJobTmpl.js
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React, { Fragment } from 'react';
 import { shape, string } from 'prop-types';
 import Header from '../organisms/Header';
@@ -7,24 +8,24 @@ import CreateJobForm from '../organisms/CreateJobForm';
 import css from 'styled-jsx/css';
 
 const styles = css`
-  /* stylelint-disable */
+	/* stylelint-disable */
 `;
 
 const CreateJobTmpl = (props) => {
-  return (
-    <Fragment>
-      <Header />
-      <CreateJobForm />
-      <Footer />
-      <style jsx>{styles}</style>
-    </Fragment>
-  );
+	return (
+		<Fragment>
+			<Header />
+			<CreateJobForm />
+			<Footer />
+			<style jsx>{styles}</style>
+		</Fragment>
+	);
 };
 
 CreateJobTmpl.propTypes = {
-  classes: shape({
-    root: string,
-  }),
+	classes: shape({
+		root: string,
+	}),
 };
 
 export default CreateJobTmpl;
--- src/components/templates/HomeTmpl.js
+++ src/components/templates/HomeTmpl.js
@@ -1,24 +1,27 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React, { Fragment } from 'react';
 import Header from '../organisms/Header';
-import Footer from '../organisms/Footer';
 import Hero from '../organisms/Hero';
+import JobListing from '../organisms/JobListing';
+import Footer from '../organisms/Footer';
 import css from 'styled-jsx/css';
 
 const styles = css`
-  /* stylelint-disable */
+	/* stylelint-disable */
 `;
 
 const HomeTmpl = (props) => {
-  return (
-    <Fragment>
-      <Header />
-      <Hero type="type-1" />
-      <Hero type="type-2" />
-      <Hero type="type-3" />
-      <Footer />
-      <style jsx>{styles}</style>
-    </Fragment>
-  );
+	return (
+		<Fragment>
+			<Header />
+			<Hero type="type-1" />
+			<JobListing />
+			<Hero type="type-2" />
+			<Hero type="type-3" />
+			<Footer />
+			<style jsx>{styles}</style>
+		</Fragment>
+	);
 };
 
 HomeTmpl.propTypes = {};
