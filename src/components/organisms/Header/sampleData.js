00bc63eb7f41e6f6629fae5562e7d5f0cbe0b1d9
Hieu Nguyen <nnth83@gmail.com>
2022-07-27 12:56:22 +0700

    - Small tweaks

--- src/components/organisms/Header/sampleData.js
+++ src/components/organisms/Header/sampleData.js
@@ -1,12 +1,12 @@
 import CreateJobLink from '../CreateJobForm/createJobtLink';
 
 const menuItems = new Map()
-  .set('Browse contest', '/search-job')
-  .set('Find a artist', '/search-artist')
+  .set('Browse jobs', '/search-job')
+  .set('Find an artist', '/search-artist')
   .set('How it works', '/how-it-works')
   .set('Submit Job', {
     path: '/create-job',
-    component: CreateJobLink,
+    component: CreateJobLink
   });
 
 export const DEFAULT_LINKS = new Map().set('menu', menuItems);
