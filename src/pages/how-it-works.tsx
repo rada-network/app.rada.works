ac1221dde8cba94dd218beea6119ade366bf5081
Qvv <quynhvv@joomsolutions.com>
2022-08-08 13:55:43 +0700

    For testing with next-auth > session.

--- src/pages/how-it-works.tsx
+++ src/pages/how-it-works.tsx
@@ -1,5 +1,13 @@
-function HowItWorks() {
-  return <div>How it works...</div>;
-}
+import { NextPage } from 'next';
+import { useSession } from 'next-auth/react';
+
+const HowItWorks: NextPage = () => {
+  const { status } = useSession();
+  console.log(status);
+
+  console.log('I am a nextjs > page...');
+
+  return '[Coming soon] How it works...';
+};
 
 export default HowItWorks;
