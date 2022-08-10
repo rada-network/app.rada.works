d8c66ffc41241c662cc58916d7ab796ca182d9da
Kien Duong <kienduong.hust@gmail.com>
2022-08-10 13:31:12 +0700

    add star preview in jobDetail

--- src/components/organisms/JobDetail/jobDetail.tsx
+++ src/components/organisms/JobDetail/jobDetail.tsx
@@ -4,7 +4,7 @@ import { useTranslation } from 'next-i18next';
 import { Tabs, TabList, Tab, TabPanels, TabPanel } from '@reach/tabs';
 import '@reach/tabs/styles.css';
 import { useJob } from '../../../hooks/JobList';
-import { formatDate, formatEndDate } from 'src/libs/useFunc';
+import { formatDate, formatEndDate, subString } from 'src/libs/useFunc';
 import Button from 'src/components/atoms/Button';
 import classes from './jobDetail.module.css';
 import { Brief } from './brief';
@@ -56,7 +56,22 @@ const JobDetail = (props: { slug: string }) => {
                   src="https://picsum.photos/200"
                 />
                 <div className={'text-sm font-medium'}>
-                  {data?.user_id?.email}
+                  {subString({
+                    str: data?.job?.[0]?.user_created?.email,
+                    start: 5,
+                    end: 3
+                  })}
+                </div>
+                <div className="flex items-center">
+                  <img
+                    className="w-5 h-5 text-yellow-400"
+                    src="/star.svg"
+                    alt="star"
+                  />
+                  <p className="ml-2 text-sm font-bold text-gray-900 dark:text-white">
+                    4.95
+                  </p>
+                  <span className="w-1 h-1 mx-1.5 bg-gray-500 rounded-full dark:bg-gray-400" />
                 </div>
                 <span className={'opacity-70 text-xs ml-2 -mb-0.5'}>
                   Posted 16 days ago
