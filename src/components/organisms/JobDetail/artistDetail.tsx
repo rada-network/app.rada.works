1c49fa3574bfcb73fb2066d58b3edd5d01da5692
Trung Chu <trungch@joomsolutions.com>
2022-08-11 16:57:48 +0700

    Update style
    Update style

--- src/components/organisms/JobDetail/artistDetail.tsx
+++ src/components/organisms/JobDetail/artistDetail.tsx
@@ -6,7 +6,7 @@ export const ArtistDetail = (props: { data: any }) => {
     <Fragment>
       <div className="bg-gray-50 dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-lg mb-6">
         <div className="border-b border-gray-200 dark:border-gray-700 py-3 px-6">
-          <h3 className="text-gray-700 dark:text-white text-lg font-semibold">
+          <h3 className="text-gray-700 dark:text-white text-lg font-medium">
             Artist In Contest (39)
           </h3>
         </div>
--- src/components/organisms/JobDetail/joinContest.tsx
+++ src/components/organisms/JobDetail/joinContest.tsx
@@ -7,7 +7,7 @@ export const JoinContest = (props: any) => {
         <span className="bg-gray-200 dark:bg-gray-700 h-12 w-12 flex items-center justify-center rounded-full mr-4">
           <img src="/ico-question.svg" alt="info" className="w-5 h-5" />
         </span>
-        <h3 className="text-lg text-gray-700 dark:text-white font-semibold">
+        <h3 className="text-lg text-gray-700 dark:text-white font-medium">
           How to joinContest
         </h3>
       </div>
