cd804d4783d835ba7121bf78a4693924d275457b
Qvv <quynhvv@joomsolutions.com>
2022-09-06 13:19:07 +0700

    Fine-tunning

--- src/components/atoms/Button/button.js
+++ src/components/atoms/Button/button.js
@@ -1,6 +1,6 @@
 import React, { useRef } from 'react';
 import { useButton } from 'react-aria';
-import { oneOf, shape, string, bool } from 'prop-types';
+import { oneOf, shape, string, bool, node, func } from 'prop-types';
 
 import { useStyle } from '../../classify';
 import defaultClasses from './button.module.css';
@@ -8,16 +8,6 @@ import defaultClasses from './button.module.css';
 const getRootClassName = (priority, negative) =>
   `root_${priority}Priority${negative ? 'Negative' : ''}`;
 
-/**
- * A component for buttons.
- *
- * @typedef Button
- * @kind functional component
- *
- * @param {props} props React component props
- *
- * @returns {React.Element} A React component that displays a single button.
- */
 const Button = (props) => {
   const {
     children,
@@ -55,27 +45,8 @@ const Button = (props) => {
   );
 };
 
-/**
- * Props for {@link Button}
- *
- * @typedef props
- *
- * @property {Object} classes An object containing the class names for the
- * Button component.
- * @property {string} classes.content classes for the button content
- * @property {string} classes.root classes for root container
- * @property {string} classes.root_highPriority classes for Button if
- * high priority.
- * @property {string} classes.root_lowPriority classes for Button if
- * low priority.
- * @property {string} classes.root_normalPriority classes for Button if
- * normal priority.
- * @property {string} priority the priority/importance of the Button
- * @property {string} type the type of the Button
- * @property {bool} negative whether the button should be displayed in red for a negative action
- * @property {bool} disabled is the button disabled
- */
 Button.propTypes = {
+  children: node,
   classes: shape({
     content: string | undefined,
     root: string | undefined,
@@ -86,7 +57,8 @@ Button.propTypes = {
   priority: oneOf(['high', 'low', 'normal']).isRequired,
   type: oneOf(['button', 'reset', 'submit']).isRequired,
   negative: bool,
-  disabled: bool
+  disabled: bool,
+  onPress: func
 };
 
 Button.defaultProps = {
