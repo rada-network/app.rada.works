f01f94ae272f010f26bb2e4eced5efd02124ec24
Trung Chu <trungch@joomsolutions.com>
2022-09-07 10:21:24 +0700

    Update style
    Update style

--- src/components/organisms/User/Modal/modal.tsx
+++ src/components/organisms/User/Modal/modal.tsx
@@ -28,9 +28,9 @@ const Modal = (props: { connect: any }) => {
         onClick={() => setShowModal(true)}
         type="button"
         data-modal-toggle="crypto-modal"
-        className="text-gray-900 bg-white hover:bg-gray-100 border border-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-600 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700"
+        className="text-gray-900 bg-white hover:bg-gray-100 border border-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm px-3 py-2 text-center inline-flex items-center dark:focus:ring-gray-600 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700"
       >
-        Login
+        Connect wallet
       </button>
       {showModal && (
         <div
