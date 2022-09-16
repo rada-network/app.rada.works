ac89d4c5c2ad4a93736c349bbcdbc4e853517832
Trung Chu <trungch@joomsolutions.com>
2022-09-16 10:22:12 +0700

    Improve responsive style
    Improve responsive style

--- src/components/organisms/Campaign/Details/details.js
+++ src/components/organisms/Campaign/Details/details.js
@@ -155,9 +155,9 @@ const Details = (props) => {
               dangerouslySetInnerHTML={{ __html: campaign.description }}
             />
 
-            <div className="flex flex-wrap items-stretch rounded-lg p-4 mb-12 border border-2 border-dashed dark:border-gray-800">
-              <div className="bg-green-50 border border-green-600 text-green-700 shadow-md rounded-lg flex flex-col items-center justify-center mb-4 md:mb-0 px-8 text-center basis-full md:basis-auto">
-                <strong className="text-6xl tracking-tight">
+            <div className="flex flex-wrap items-stretch rounded-lg p-2 lg:p-4 mb-12 border border-2 border-dashed dark:border-gray-800">
+              <div className="bg-green-50 border border-green-600 text-green-700 shadow-md rounded-lg flex flex-col items-center justify-center mb-4 lg:mb-0 px-8 py-3 lg:py-0 text-center basis-full lg:basis-auto">
+                <strong className="text-6xl tracking-tight mb-1">
                   {campaign.discount_value}
                   <span className="leading-none font-light">%</span>
                 </strong>
@@ -165,13 +165,13 @@ const Details = (props) => {
                   Discount
                 </span>
               </div>
-              <div className="md:pl-6 w-full">
+              <div className="lg:pl-6 w-full flex-1">
                 <div className="mb-5">
                   <ul className="flex flex-wrap list-none m-0 p-0">
                     <li className="flex items-center md:w-1/2 m-0 mb-3 p-0">
                       <span className="flex text-gray-500 items-center mr-2">
                         <svg
-                          className="w-4 h-4 fill-gray-500 mr-2"
+                          className="w-4 h-4 fill-gray-500 mr-2.5"
                           xmlns="http://www.w3.org/2000/svg"
                         >
                           <path d="M8 7a.5.5 0 0 1 .5.5V9H10a.5.5 0 0 1 0 1H8.5v1.5a.5.5 0 0 1-1 0V10H6a.5.5 0 0 1 0-1h1.5V7.5A.5.5 0 0 1 8 7z" />
@@ -185,7 +185,7 @@ const Details = (props) => {
                     <li className="flex items-center md:w-1/2 m-0 mb-3 p-0">
                       <span className="flex items-center text-gray-500 mr-2">
                         <svg
-                          className="w-4 h-4 fill-gray-500 mr-2"
+                          className="w-4 h-4 fill-gray-500 mr-2.5"
                           xmlns="http://www.w3.org/2000/svg"
                         >
                           <path d="M6.146 7.146a.5.5 0 0 1 .708 0L8 8.293l1.146-1.147a.5.5 0 1 1 .708.708L8.707 9l1.147 1.146a.5.5 0 0 1-.708.708L8 9.707l-1.146 1.147a.5.5 0 0 1-.708-.708L7.293 9 6.146 7.854a.5.5 0 0 1 0-.708z" />
@@ -200,7 +200,7 @@ const Details = (props) => {
                     <li className="w-1/2 m-0 p-0">
                       <span className="flex items-center">
                         <svg
-                          className="w-4 h-4 fill-gray-500 mr-2"
+                          className="w-4 h-4 fill-gray-500 mr-2.5"
                           xmlns="http://www.w3.org/2000/svg"
                         >
                           <path d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm7.5-6.923c-.67.204-1.335.82-1.887 1.855A7.97 7.97 0 0 0 5.145 4H7.5V1.077zM4.09 4a9.267 9.267 0 0 1 .64-1.539 6.7 6.7 0 0 1 .597-.933A7.025 7.025 0 0 0 2.255 4H4.09zm-.582 3.5c.03-.877.138-1.718.312-2.5H1.674a6.958 6.958 0 0 0-.656 2.5h2.49zM4.847 5a12.5 12.5 0 0 0-.338 2.5H7.5V5H4.847zM8.5 5v2.5h2.99a12.495 12.495 0 0 0-.337-2.5H8.5zM4.51 8.5a12.5 12.5 0 0 0 .337 2.5H7.5V8.5H4.51zm3.99 0V11h2.653c.187-.765.306-1.608.338-2.5H8.5zM5.145 12c.138.386.295.744.468 1.068.552 1.035 1.218 1.65 1.887 1.855V12H5.145zm.182 2.472a6.696 6.696 0 0 1-.597-.933A9.268 9.268 0 0 1 4.09 12H2.255a7.024 7.024 0 0 0 3.072 2.472zM3.82 11a13.652 13.652 0 0 1-.312-2.5h-2.49c.062.89.291 1.733.656 2.5H3.82zm6.853 3.472A7.024 7.024 0 0 0 13.745 12H11.91a9.27 9.27 0 0 1-.64 1.539 6.688 6.688 0 0 1-.597.933zM8.5 12v2.923c.67-.204 1.335-.82 1.887-1.855.173-.324.33-.682.468-1.068H8.5zm3.68-1h2.146c.365-.767.594-1.61.656-2.5h-2.49a13.65 13.65 0 0 1-.312 2.5zm2.802-3.5a6.959 6.959 0 0 0-.656-2.5H12.18c.174.782.282 1.623.312 2.5h2.49zM11.27 2.461c.247.464.462.98.64 1.539h1.835a7.024 7.024 0 0 0-3.072-2.472c.218.284.418.598.597.933zM10.855 4a7.966 7.966 0 0 0-.468-1.068C9.835 1.897 9.17 1.282 8.5 1.077V4h2.355z" />
