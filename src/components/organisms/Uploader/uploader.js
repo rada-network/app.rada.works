50563bb127cd4c7a72e4879c702d608aa4e202c1
Qvv <quynhvv@joomsolutions.com>
2022-09-28 16:07:38 +0700

    [Processing more] Campaign > Uploading cover and thumb images.

--- src/components/organisms/Uploader/uploader.js
+++ src/components/organisms/Uploader/uploader.js
@@ -8,11 +8,12 @@ import Tus from '@uppy/tus';
 import '@uppy/core/dist/style.css';
 import '@uppy/drag-drop/dist/style.css';
 import BrowserPersistence from '../../../utils/simplePersistence';
-import {
+/*import {
   importFileFunc,
   deleteFileFunc
-} from '../../../hooks/Uploader/api.gql';
+} from '../../../hooks/Uploader/api.gql';*/
 import { ellipsify } from '../../../utils/strUtils';
+import { getPageFiles } from 'next/dist/server/get-page-files';
 
 const Uploader = (props) => {
   const {
@@ -61,22 +62,32 @@ const Uploader = (props) => {
 
   const { t } = useTranslation('uploader');
 
-  const addFile = (file) => {
+  useEffect(() => {
+    //refresh local storage
+    const storage = new BrowserPersistence();
+    storage.removeItem(storageKeyName);
+  }, []);
+
+  const addFiles = (files) => {
     const storage = new BrowserPersistence();
     let uploadedFiles = storage.getItem(storageKeyName);
     if (uploadedFiles === undefined) {
       uploadedFiles = [];
     }
-    uploadedFiles.push(file);
+    files.map((file) => {
+      const found = uploadedFiles.some((el) => el.id === file.id);
+      if (!found) uploadedFiles.push(file);
+    });
+    //save
     storage.setItem(storageKeyName, uploadedFiles, 24 * 60 * 60);
   };
 
   const uppy = useUppy(() => {
     return new Uppy({
       id,
-      autoProceed: true,
-      allowMultipleUploadBatches: true,
       debug: true,
+      autoProceed: true,
+      allowMultipleUploadBatches: allowMultipleFiles,
       restrictions: {
         allowedFileTypes,
         maxNumberOfFiles,
@@ -90,51 +101,19 @@ const Uploader = (props) => {
       }
     });
   });
+
   uppy.on('upload', (data) => {
     setUploadingState(uploaderContainerId);
   });
-  uppy.on('upload-success', (file, response) => {
-    setTimeout(async function () {
-      // import as directus file
-      await importFileFunc({
-        url: response.uploadURL,
-        data: {
-          title: file.name,
-          type: file.type,
-          storage: 'local',
-          folder: storageFolder,
-          filename_download: file.name,
-          uploaded_on: new Date(),
-          modified_on: new Date()
-        }
-      }).then(function (rs) {
-        console.log('Imported File: ', rs);
-        if (rs.data && rs.data.import_file) {
-          // saving uploaded file to local storage
-          const directusFile = {
-            id: rs.data.import_file.id,
-            storage: rs.data.import_file.storage,
-            filename_download: rs.data.import_file.filename_download,
-            uploaded_on: rs.data.import_file.uploaded_on,
-            modified_on: rs.data.import_file.modified_on
-          };
-          file.directus_file = directusFile;
-          addFile(file.directus_file);
 
-          // update preview
-          updatePreview(
-            t,
-            storageKeyName,
-            previewContainerId,
-            uppy,
-            file,
-            response.uploadURL
-          );
-        }
-      });
-    }, 1500);
-  });
   uppy.on('complete', (result) => {
+    const uploadedFiles = result.successful.length ? result.successful : [];
+    if (uploadedFiles.length) {
+      // Save uploaded files to local storage
+      addFiles(uploadedFiles);
+      // Generate preview
+      generatePreview(t, previewContainerId, storageKeyName, uppy);
+    }
     cleanUploadingState(uploaderContainerId);
   });
 
@@ -156,7 +135,7 @@ const Uploader = (props) => {
             browse: t('browse')
           }
         }}
-        note={`${t('Allowed file types:')} ${allowedFileTypes.join(', ')}`}
+        note={`${t('Allows file types:')} ${allowedFileTypes.join(', ')}`}
       />
       <ul id={previewContainerId} className={`${classes.previewContainer}`} />
     </div>
@@ -180,75 +159,65 @@ const cleanUploadingState = (uploaderContainerId) => {
   }, 1000);
 };
 
-const updatePreview = (
-  t,
-  storageKeyName,
-  previewElId,
-  uppy,
-  file,
-  uploadedUrl
-) => {
-  if (!document.getElementById(file.directus_file.id)) {
-    // preview item
-    const item = document.createElement('li');
-    item.id = file.directus_file.id;
-    item.className = classes.previewItem;
+const generatePreview = (t, previewContainerId, storageKeyName, uppy) => {
+  const storage = new BrowserPersistence();
+  const uploadedFiles = storage.getItem(storageKeyName);
+  if (uploadedFiles) {
+    uploadedFiles.map((file) => {
+      if (!document.getElementById(file.id)) {
+        // preview item
+        const item = document.createElement('li');
+        item.id = file.id;
+        item.className = classes.previewItem;
 
-    if (/(jpe?g|png|gif|bmp)$/i.test(file.extension)) {
-      // create thumb
-      const img = new Image();
-      // img.width = 100;
-      img.alt = file.name;
-      img.src = uploadedUrl;
-      img.className = classes.thumb;
-      item.appendChild(img);
-    }
+        if (/(jpe?g|png|gif|bmp)$/i.test(file.extension)) {
+          // create thumb
+          const img = new Image();
+          img.alt = file.name;
+          img.src = file.uploadURL;
+          img.className = classes.previewImage;
+          item.appendChild(img);
+        }
 
-    // create file name with link
-    const fileLink = document.createElement('a');
-    fileLink.href = uploadedUrl;
-    fileLink.target = '_blank';
-    fileLink.className = classes.fileUploaded;
-    const fileLinkText = document.createTextNode(
-      ellipsify({ str: file.name, start: 5, end: 4 })
-    );
-    fileLink.appendChild(fileLinkText);
-    item.appendChild(fileLink);
+        // create file name with link
+        const fileLink = document.createElement('a');
+        fileLink.href = file.uploadUrl;
+        fileLink.target = '_blank';
+        fileLink.className = classes.fileUploaded;
+        const fileLinkText = document.createTextNode(
+          ellipsify({ str: file.name, start: 5, end: 4 })
+        );
+        fileLink.appendChild(fileLinkText);
+        item.appendChild(fileLink);
 
-    // create remove button
-    if (item) {
-      const rmBtn = document.createElement('span');
-      rmBtn.className = classes.btnDelete;
-      const rmBtnText = document.createTextNode(t('Remove'));
-      rmBtn.appendChild(rmBtnText);
+        // create remove button
+        if (item.id) {
+          const rmBtn = document.createElement('span');
+          rmBtn.className = classes.btnRemove;
+          const rmBtnText = document.createTextNode(t('Remove'));
+          rmBtn.appendChild(rmBtnText);
 
-      rmBtn.onclick = (e) => {
-        //delete the related directus file
-        deleteFileFunc(file.directus_file.id).then(function (rs) {
-          console.log(rs);
+          rmBtn.onclick = (e) => {
+            // remove file from uppy
+            uppy.removeFile(file.id);
 
-          // remove file from uppy
-          uppy.removeFile(file.id);
+            // remove file from local storage
+            if (uploadedFiles) {
+              const newFiles = uploadedFiles.filter((obj) => {
+                return obj.id !== file.id;
+              });
+              storage.setItem(storageKeyName, newFiles);
+            }
 
-          //delete from local storage
-          const storage = new BrowserPersistence();
-          const uploadedFiles = storage.getItem(storageKeyName);
-          if (uploadedFiles) {
-            const newIds = uploadedFiles.filter((val) => {
-              return val !== file.directus_file.id;
-            });
-            storage.setItem(storageKeyName, newIds);
-          }
+            //clean preview element on DOM
+            document.getElementById(file.id).remove();
+          };
 
-          //clean preview element on DOM
-          document.getElementById(file.directus_file.id).remove();
-        });
-      };
-
-      item.appendChild(rmBtn);
-    }
-
-    document.getElementById(previewElId).appendChild(item);
+          item.appendChild(rmBtn);
+        }
+        document.getElementById(previewContainerId).appendChild(item);
+      }
+    });
   }
 };
 
