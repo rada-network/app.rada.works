b9f3d78fda607976e3de5e1e223fa33a0f28b043
Qvv <quynhvv@joomsolutions.com>
2022-09-28 17:10:23 +0700

    Tuning

--- src/pages/api/auth/[...nextauth].tsx
+++ src/pages/api/auth/[...nextauth].tsx
@@ -1,8 +1,8 @@
 import NextAuth from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
 import GoogleProvider from 'next-auth/providers/google';
-import GithubProvider from 'next-auth/providers/github';
-import FacebookProvider from 'next-auth/providers/facebook';
+// import GithubProvider from 'next-auth/providers/github';
+// import FacebookProvider from 'next-auth/providers/facebook';
 import TwitterProvider from 'next-auth/providers/twitter';
 
 import {
