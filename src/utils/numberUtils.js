7f5b6dd259e16f3bb8727c4fc0e340675678dd0d
Qvv <quynhvv@joomsolutions.com>
2022-09-29 11:01:08 +0700

    Added utils functions for numbers

---
+++ src/utils/numberUtils.js
@@ -0,0 +1,22 @@
+export const kFormatter = (number) => {
+  return Math.abs(number) > 999
+    ? Math.sign(number) * (Math.abs(number) / 1000).toFixed(1) + 'k'
+    : Math.sign(number) * Math.abs(number);
+};
+
+export const formatBytes = (bytes, decimals = 2) => {
+  if (!+bytes) return '0 Bytes';
+
+  const k = 1024;
+  const dm = decimals < 0 ? 0 : decimals;
+  const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+
+  const i = Math.floor(Math.log(bytes) / Math.log(k));
+
+  return `${parseFloat((bytes / Math.pow(k, i)).toFixed(dm))} ${sizes[i]}`;
+};
+
+export default {
+  kFormatter,
+  formatBytes
+};
