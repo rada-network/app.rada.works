66b5603ed4aa2b383c3c88a191a02531fef0b617
Kien Duong <kienduong.hust@gmail.com>
2022-10-08 22:25:57 +0700

    fixed check retweet

--- src/hooks/Campaign/Rewards/useTwitterFollow.js
+++ src/hooks/Campaign/Rewards/useTwitterFollow.js
@@ -3,9 +3,6 @@ import { TwitterFollow } from './useTwitter';
 
 export default (props) => {
   const { user_id, owner_id } = props;
-  console.log('====================================');
-  console.log('user_id', user_id);
-  console.log('====================================');
   const handleCheckTwitterFollow = useCallback(async () => {
     const data = await TwitterFollow({ user_id, owner_id });
     return data;
