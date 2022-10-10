db037985048f6f167a2c3cc1db529776bafe7d34
Kien Duong <kienduong.hust@gmail.com>
2022-10-10 16:15:12 +0700

    add define id on session

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -6,9 +6,11 @@ declare module 'next-auth' {
    * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
    */
   interface Session {
+    id: string;
     access_token: string;
     error: string;
     user: {
+      id: string;
       /** The user's postal address. */
       address: string;
     } & DefaultSession['user'];
