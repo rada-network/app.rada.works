c141788c77bc2de22224d39ce42326f79c9c9c07
Kien Duong <kienduong.hust@gmail.com>
2022-10-10 16:07:51 +0700

    add Module Augmentation next-auth

---
+++ types/next-auth.d.ts
@@ -0,0 +1,24 @@
+import { DefaultSession } from 'next-auth';
+import { DefaultJWT } from 'next-auth/jwt';
+
+declare module 'next-auth' {
+  /**
+   * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
+   */
+  interface Session {
+    test: string;
+    access_token: string;
+    user: {
+      /** The user's postal address. */
+      address: string;
+    } & DefaultSession['user'];
+  }
+}
+declare module 'next-auth/jwt' {
+  /** Returned by the `jwt` callback and `getToken`, when using JWT sessions */
+  interface JWT extends Record<string, unknown>, DefaultJWT {
+    /** OpenID ID Token */
+    idToken?: string;
+    error: string;
+  }
+}
